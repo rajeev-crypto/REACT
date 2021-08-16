e9db03059825053546c23b7c2251426834456350
rajeev <rajeevkushwaha@gamil.com>
2021-08-17 03:24:58 +0530

    creating class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CC from './CC'
 
 function App() {
-  
+
   return (
+  <div className = "App">
     <h1> Hello wOrld </h1>
+
+    <CC/>
+  </div>
   );
 }
 
---
+++ src/CC.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+class CC extends React.Component{
+
+	render(){
+		return <div>
+		  <h1> Hello World Using Class Component </h1> 
+		 <h1> Hii </h1>
+		 </div>
+	}
+}
+export default CC;
